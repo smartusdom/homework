ce5955fb7f962d460357c58ac0446b706f1d9e33
Ruslan G <rusgilm@mail.ru>
2020-12-28 15:13:27 +0300

    uniq

--- demo/output.tf
+++ demo/output.tf
@@ -1,7 +1,7 @@
-output "public_ip" {
+output "develop_public_ip" {
   value = "${aws_instance.develop.public_ip}"
 }
 
-output "public_ip" {
+output "web_public_ip" {
   value = "${aws_instance.web.public_ip}"
 }
